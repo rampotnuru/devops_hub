7aad1c20a19a52311bac749da6a38e448e48b508
rampotnuru <pavanram.potnuru@gmail.com>
2026-07-08 11:16:39 +0000

    Added a script to install nginx on EC2 server

---
+++ scripts/ngnix_installation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo yum update -y
+sudo yum install nginx -y
+
+sudo systemctl enable nginx
+sudo systemctl start nginx
+
+curl http://localhost:80
+
+echo "nginx installation completed!"
+
